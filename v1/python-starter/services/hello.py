fec43809b8cb2c607a2de7bb562975f5030db482
David Moore <4121492+davemooreuws@users.noreply.github.com>
2024-03-05 13:20:13 +1100

    python v1 (#6)
    * python v1
    
    * run python dev unbuffered
    
    ---------
    
    Co-authored-by: Tim Holm <tim.holm@nitric.io>

--- v1/python-starter/services/hello.py
+++ v1/python-starter/services/hello.py
@@ -1,11 +1,10 @@
 from nitric.resources import api
 from nitric.application import Nitric
-from nitric.faas import HttpContext
+from nitric.context import HttpContext
 
-main_api = api("main")
+main = api("main")
 
-
-@main_api.get("/hello/:name")
+@main.get("/hello/:name")
 async def hello_world(ctx: HttpContext):
     name = ctx.req.params['name']
 
