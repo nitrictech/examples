ce9fa964f8fa9853809bdc3db48c6313b39980b9
David Moore <davemooreuws@gmail.com>
2024-03-12 12:29:48 +1100

    feat: dart v1

---
+++ v1/dart-starter/services/hello.dart
@@ -0,0 +1,13 @@
+import 'package:nitric_sdk/nitric.dart';
+
+void main() {
+  final helloApi = Nitric.api("main");
+
+  helloApi.get("/hello/:name", (ctx) async {
+    final name = ctx.req.pathParams["name"]!;
+
+    ctx.resp.body = "Hello $name";
+
+    return ctx;
+  });
+}
