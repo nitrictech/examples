0808366b7218555c3967404517d01f97b1e73f2c
David Moore <davemooreuws@gmail.com>
2024-06-18 13:17:18 +1000

    add go template api example

--- v1/golang-starter/services/hello/main.go
+++ v1/golang-starter/services/hello/main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/nitrictech/go-sdk/handler"
+	"github.com/nitrictech/go-sdk/nitric"
 )
 
 func main() {
-	// PUT YOUR TEST CODE HERE :)
+	api, err := nitric.NewApi("main")
+	if err != nil {
+		return
+	}
+
+	api.Get("/hello/:name", func(ctx *handler.HttpContext, next handler.HttpHandler) (*handler.HttpContext, error) {
+		name := ctx.Request.PathParams()["name"]
+		ctx.Response.Body = []byte(fmt.Sprintf("Hello %s", name))
+
+		return next(ctx)
+	})
+
+	if err := nitric.Run(); err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Println("Service has started")
 }
