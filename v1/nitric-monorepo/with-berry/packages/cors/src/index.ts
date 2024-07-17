0fb146040ba81df868ec07ecfb185a1478fdba12
David Moore <davemooreuws@gmail.com>
2024-07-17 11:21:27 +1000

    nitric with yarn berry workspaces

---
+++ v1/nitric-monorepo/with-berry/packages/cors/src/index.ts
@@ -0,0 +1,26 @@
+import { HttpMiddleware } from "@nitric/sdk";
+
+export const optionsHandler: HttpMiddleware = (ctx) => {
+  ctx.res.headers["Content-Type"] = ["text/html; charset=ascii"];
+  ctx.res.status = 200;
+  ctx.res.body = "OK";
+
+  return ctx;
+};
+
+export const cors: HttpMiddleware = (ctx, next) => {
+  ctx.res.headers["Access-Control-Allow-Origin"] = ["*"];
+  ctx.res.headers["Access-Control-Allow-Headers"] = [
+    "Origin, Content-Type, Accept, Authorization",
+  ];
+  ctx.res.headers["Access-Control-Allow-Methods"] = [
+    "GET, PUT, POST, OPTIONS, DELETE",
+  ];
+  ctx.res.headers["Access-Control-Allow-Credentials"] = ["true"];
+
+  if (next) {
+    return next(ctx);
+  }
+
+  return ctx;
+};
