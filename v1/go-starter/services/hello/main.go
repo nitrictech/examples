1178d501a2d28d632e266ea60ec4be4a1e9a147d
Tim Holm <tim.holm@nitric.io>
2024-10-03 17:22:10 +1000

    update go-starter project (#61)

--- v1/go-starter/services/hello/main.go
+++ v1/go-starter/services/hello/main.go
@@ -3,27 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/nitrictech/go-sdk/handler"
 	"github.com/nitrictech/go-sdk/nitric"
+	"github.com/nitrictech/go-sdk/nitric/apis"
 )
 
 func main() {
-	api, err := nitric.NewApi("main")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	api := nitric.NewApi("main")
 
-	api.Get("/hello/:name", func(ctx *handler.HttpContext, next handler.HttpHandler) (*handler.HttpContext, error) {
+	api.Get("/hello/:name", func(ctx *apis.Ctx) {
 		name := ctx.Request.PathParams()["name"]
 		ctx.Response.Body = []byte(fmt.Sprintf("Hello %s", name))
-
-		return next(ctx)
 	})
 
-	if err := nitric.Run(); err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("Service has started")
+	nitric.Run()
 }
