e9e358db703e1430ed0dd2905f39ba0e4436b8f2
Ryan Cartwright <fryzee.cartwright@gmail.com>
2024-11-06 14:32:09 +1100

    use upload redirect instead of writing to bucket

--- v1/blender-render/batches/renderer.py
+++ v1/blender-render/batches/renderer.py
@@ -4,7 +4,7 @@ import glob
 
 from nitric.context import JobContext
 from nitric.application import Nitric
-from src.resources import rendered_bucket, renderer_job, blend_bucket
+from common.resources import rendered_bucket, renderer_job, blend_bucket
 
 readable_blend_bucket = blend_bucket.allow("read")
 writeable_rendered_bucket = rendered_bucket.allow("write")
@@ -16,34 +16,40 @@ async def render_image(ctx: JobContext):
 
   blend_key = ctx.req.data["key"]
 
+  print(blend_key)
+
   # Register the blender binary
   blender_bin = "blender"
 
   if os.path.isfile(blender_bin):    
     bpy.app.binary_path = blender_bin
   else:
+    print("unable to find blender path")
     ctx.res.success = False
     return ctx
 
   # load the file from a bucket to a local file
-  blend_file = await readable_blend_bucket.file(f"{blend_key}.blend").read()
+  blend_file = await readable_blend_bucket.file(f"blend-{blend_key}.blend").read()
 
-  with open("input", "wb") as f:
+  with open("input.blend", "wb") as f:
     f.write(blend_file)
 
-  bpy.ops.wm.open_mainfile(filepath="input")
+  bpy.ops.wm.open_mainfile(filepath="input.blend")
 
-  raw_metadata = await readable_blend_bucket.file(f"{blend_key}.metadata.json").read()
+  try:
+    raw_metadata = await readable_blend_bucket.file(f"metadata-{blend_key}.json").read()
+  except:
+    raw_metadata = {}
 
   metadata = json.loads(raw_metadata)
 
   bpy.context.scene.render.filepath = blend_key
-  bpy.context.scene.render.engine = metadata.get('engine')
-  bpy.context.scene.cycles.device = metadata.get('device')
-  bpy.context.scene.render.image_settings.file_format = metadata.get('file_format')
-  bpy.context.scene.render.fps = metadata.get('fps')
+  bpy.context.scene.render.engine = metadata.get('engine', 'CYCLES')
+  bpy.context.scene.cycles.device = metadata.get('device', 'GPU')
+  bpy.context.scene.render.image_settings.file_format = metadata.get('file_format', 'PNG')
+  bpy.context.scene.render.fps = metadata.get('fps', '0')
 
-  if metadata.get('animate'):
+  if metadata.get('animate', False):
     bpy.ops.render.render(animation=True)
   else:
     bpy.ops.render.render(write_still=True)
--- v1/blender-render/services/main.py
+++ v1/blender-render/services/main.py
@@ -1,16 +1,14 @@
 import json
-import requests
 
 from nitric.application import Nitric
 from nitric.context import HttpContext
 from nitric.resources import BucketNotificationContext
 
-from src.resources import rendered_bucket, main_api, blend_bucket, renderer_job
+from common.resources import rendered_bucket, main_api, blend_bucket, renderer_job
 
 readable_rendered_bucket = rendered_bucket.allow("read")
 readable_writeable_blend_bucket = blend_bucket.allow("write", "read")
 submittable_renderer_job = renderer_job.allow("submit")
-
 
 @main_api.get("/render/:file")
 async def get_render(ctx: HttpContext):
@@ -23,19 +21,11 @@ async def get_render(ctx: HttpContext):
 
     return ctx
     
-@main_api.post("/:blend")
+@main_api.put("/:blend")
 async def write_render(ctx: HttpContext):
     blend_scene_key = ctx.req.params["blend"]
     
-    # Use upload url to get around write limit
-    upload_url = await readable_writeable_blend_bucket.file(f"{blend_scene_key}.blend").upload_url()
-    
-    resp = requests.post(upload_url, data=ctx.req.data)
-    if resp.status_code >= 200 and resp.status_code < 300:
-        ctx.res.status = resp.status_code
-        ctx.res.body = resp.text
-        return ctx
-    
+    # Write the blend scene rendering settings
     raw_metadata = {
         "file_format": str(ctx.req.query.get('file_format', ['PNG'])[0]),
         "fps": int(ctx.req.query.get('fps', [0])[0]),
@@ -43,26 +33,23 @@ async def write_render(ctx: HttpContext):
         "engine": str(ctx.req.query.get('engine', ['CYCLES'])[0]),
         "animate": bool(ctx.req.query.get('animate', [False])[0]),
     }
-
     metadata = bytes(json.dumps(raw_metadata), encoding="utf-8")
 
-    await readable_writeable_blend_bucket.file(f"{blend_scene_key}.metadata.json").write(metadata)
+    await readable_writeable_blend_bucket.file(f"metadata-{blend_scene_key}.json").write(metadata)
+
+    # Write the blend scene to the bucket using an upload URL
+    blend_upload_url = await readable_writeable_blend_bucket.file(f"blend-{blend_scene_key}.blend").upload_url()
+
+    ctx.res.headers["Location"] = blend_upload_url
+    ctx.res.status = 307
 
     return ctx
 
 
-@blend_bucket.on("write", "*")
+@blend_bucket.on("write", "blend-")
 async def on_written_image(ctx: BucketNotificationContext):
-    key_without_extension = ctx.req.key.split(".")[0]
+    key_without_extension = ctx.req.key.split(".")[0][6:]
 
-    # Ensure {scene}.metadata.json exists and {scene}.blend
-    blend_exists = await readable_writeable_blend_bucket.exists(f"{key_without_extension}.blend")
-    metadata_exists = await readable_writeable_blend_bucket.exists(f"{key_without_extension}.metadata.json")
-
-    if not blend_exists or not metadata_exists:
-        ctx.res.success = False
-        return ctx
-        
     await submittable_renderer_job.submit(
         {
             "key": key_without_extension,
