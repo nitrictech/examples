4f2fe337b094973557702dd65663b2d10ab19213
Tim Holm <tim.holm@nitric.io>
2024-11-07 12:48:41 +1100

    updates.

--- v1/podcast-transcription/services/api.py
+++ v1/podcast-transcription/services/api.py
@@ -3,22 +3,33 @@ from nitric.application import Nitric
 from nitric.resources import BucketNotificationContext
 from nitric.context import HttpContext
 
-writeable_podcast_bucket = podcast_bucket.allow("write")
 readable_transcript_bucket = transcript_bucket.allow("read")
 submittable_transcribe_job = transcribe_job.allow("submit")
+writable_podcast_bucket = podcast_bucket.allow("write")
 
 @main_api.get("/podcast/:name")
 async def get_podcast(ctx: HttpContext):
     name = ctx.req.params['name']
 
-    download_url = await writeable_podcast_bucket.file(name).download_url()
+    download_url = await readable_transcript_bucket.file(f"{name}-transcript.txt").download_url()
 
     ctx.res.headers["Location"] = download_url
     ctx.res.status = 303
 
     return ctx
 
-@writeable_podcast_bucket.on("write", "*")
+@main_api.put("/audio/:name")
+async def put_audio(ctx: HttpContext):
+    name = ctx.req.params['name']
+
+    upload_url = await writable_podcast_bucket.file(name).upload_url()
+
+    ctx.res.headers["Location"] = upload_url
+    ctx.res.status = 307
+
+    return ctx
+
+@podcast_bucket.on("write", "*")
 async def on_add_podcast(ctx: BucketNotificationContext):
     await submittable_transcribe_job.submit({ "podcast_name": ctx.req.key })
 
