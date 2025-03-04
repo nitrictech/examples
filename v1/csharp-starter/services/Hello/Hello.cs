836f4034465d78de5c155bb6cd8e05e9f096c35c
Ryan Cartwright <fryzee.cartwright@gmail.com>
2025-03-04 17:12:20 +1100

    add csharp starter template

---
+++ v1/csharp-starter/services/Hello/Hello.cs
@@ -0,0 +1,14 @@
+﻿using Application = Nitric.Sdk.Nitric;
+
+var api = Application.Api("main");
+
+api.Get("/hello/:name", context =>
+{
+    var name = context.Req.PathParams["name"];
+
+    context.Res.Text($"Hello {name}!");
+
+    return context;
+});
+
+Application.Run();
